47fd49a2034a441beb37ce82aea45ccd0159aa41
MAXPOL <maxrol32@gmail.com>
2022-05-03 22:01:47 +0300

    Create firewallAsterisk.sh

---
+++ firewallAsterisk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+yum install firewalld -y
+systemctl start firewalld
+systemctl enable firewalld
+
+firewall-cmd --permanent --zone=public --add-port=5060/tcp
+firewall-cmd --permanent --zone=public --add-port=5060/udp
+firewall-cmd --permanent --zone=public --add-port=5061/tcp
+firewall-cmd --permanent --zone=public --add-port=5061/udp
+firewall-cmd --permanent --zone=public --add-port=4569/tcp
+firewall-cmd --permanent --zone=public --add-port=4569/udp
+firewall-cmd --permanent --zone=public --add-port=5038/tcp
+firewall-cmd --permanent --zone=public --add-port=5038/udp
+firewall-cmd --permanent --zone=public --add-port=80/tcp
+firewall-cmd --permanent --zone=public --add-port=80/udp
+
+firewall-cmd --reload
