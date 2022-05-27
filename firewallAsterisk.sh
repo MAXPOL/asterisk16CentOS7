94871456e945d49e498de23b6fde81c578ad5cac
MAXPOL <maxrol32@gmail.com>
2022-05-27 20:35:41 +0300

    Update firewallAsterisk.sh

--- firewallAsterisk.sh
+++ firewallAsterisk.sh
@@ -10,13 +10,9 @@ firewall-cmd --permanent --zone=public --add-port=5061/tcp
 firewall-cmd --permanent --zone=public --add-port=5061/udp
 firewall-cmd --permanent --zone=public --add-port=5160/tcp
 firewall-cmd --permanent --zone=public --add-port=5160/udp
-firewall-cmd --permanent --zone=public --add-port=4569/tcp
 firewall-cmd --permanent --zone=public --add-port=4569/udp
 firewall-cmd --permanent --zone=public --add-port=5038/tcp
 firewall-cmd --permanent --zone=public --add-port=5038/udp
 firewall-cmd --permanent --zone=public --add-port=80/tcp
-firewall-cmd --permanent --zone=public --add-port=80/udp
 firewall-cmd --permanent --zone=public --add-port=10000-20000/udp
-firewall-cmd --permanent --zone=public --add-port=10000-20000/tcp
-
 firewall-cmd --reload
